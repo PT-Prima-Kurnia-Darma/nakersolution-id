a6ab07513db0a4d53c50f14ed1cad7575740c9e5
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-06-28 22:04:46 +0700

    fix: snackbar show behind the keyboard

--- app/src/main/java/com/nakersolutionid/nakersolutionid/features/login/LoginScreen.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/features/login/LoginScreen.kt
@@ -9,11 +9,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -90,7 +93,10 @@ fun LoginScreen(
 
     Scaffold(
         snackbarHost = {
-            SnackbarHost(hostState = snackbarHostState)
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier.windowInsetsPadding(WindowInsets.ime)
+            )
         },
     ) { innerPadding ->
         // Using a Column with verticalArrangement.Center to better position the content
