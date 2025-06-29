63485eb00e0463e2b3f17bdb5df065014f3353de
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-06-30 00:07:09 +0700

    feat: added SettingsViewModel.kt into PreviewModule.kt

--- app/src/main/java/com/nakersolutionid/nakersolutionid/di/PreviewModule.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/di/PreviewModule.kt
@@ -1,11 +1,11 @@
 package com.nakersolutionid.nakersolutionid.di
 
-
 import com.nakersolutionid.nakersolutionid.domain.repository.IUserRepository
 import com.nakersolutionid.nakersolutionid.domain.usecase.UserInteraction
 import com.nakersolutionid.nakersolutionid.domain.usecase.UserUseCase
 import com.nakersolutionid.nakersolutionid.features.home.HomeViewModel
 import com.nakersolutionid.nakersolutionid.features.login.LoginViewModel
+import com.nakersolutionid.nakersolutionid.features.settings.SettingsViewModel
 import com.nakersolutionid.nakersolutionid.features.signup.SignUpViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
@@ -16,4 +16,5 @@ val previewModule = module {
     viewModel { LoginViewModel(get()) }
     viewModel { SignUpViewModel(get()) }
     viewModel { HomeViewModel(get()) }
+    viewModel { SettingsViewModel(get()) }
 }
