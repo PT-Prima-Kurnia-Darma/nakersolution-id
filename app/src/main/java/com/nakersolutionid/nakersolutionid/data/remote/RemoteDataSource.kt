983ea286ec37082d18698970043958b44e8311c1
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-06-29 14:29:21 +0700

    fix: now it will emit all the error properly on flow

--- app/src/main/java/com/nakersolutionid/nakersolutionid/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/data/remote/RemoteDataSource.kt
@@ -1,6 +1,8 @@
 package com.nakersolutionid.nakersolutionid.data.remote
 
+import com.google.gson.Gson
 import com.google.gson.JsonParser
+import com.google.gson.JsonSyntaxException
 import com.nakersolutionid.nakersolutionid.data.remote.network.ApiResponse
 import com.nakersolutionid.nakersolutionid.data.remote.network.ApiServices
 import com.nakersolutionid.nakersolutionid.data.remote.request.LoginRequest
@@ -13,6 +15,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import retrofit2.HttpException
+import java.net.SocketTimeoutException
 
 class RemoteDataSource(private val apiServices: ApiServices) {
     fun register(name: String, username: String, password: String): Flow<ApiResponse<RegisterResponse>> {
@@ -23,17 +26,23 @@ class RemoteDataSource(private val apiServices: ApiServices) {
             } catch (e: HttpException) {
                 val errorBody = e.response()?.errorBody()?.string()
                 errorBody?.let {
-                    val jsonElement = JsonParser.parseString(it)
-                    val detail = jsonElement
-                        .asJsonObject["message"]
-                        .asString
-                    emit(ApiResponse.Error(detail))
+                    try {
+                        val parsedError = Gson().fromJson(errorBody, RegisterResponse::class.java)
+                        if (parsedError != null) {
+                            emit(ApiResponse.Error(parsedError.message))
+                        } else {
+                            emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x0)"))
+                        }
+                    } catch (e: JsonSyntaxException) {
+                        emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x1)"))
+                    }
+                } ?: run {
+                    emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x2)"))
                 }
+            } catch (e: SocketTimeoutException) {
+                emit(ApiResponse.Error("Gagal terhubung, waktu tunggu koneksi habis"))
             } catch (e: Exception) {
-                e.message?.let {
-                    if (it == "timeout") emit(ApiResponse.Error("Connection timeout"))
-                }
-                emit(ApiResponse.Error("Something went wrong"))
+                emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x3)"))
             }
         }.flowOn(Dispatchers.IO)
     }
@@ -46,17 +55,23 @@ class RemoteDataSource(private val apiServices: ApiServices) {
             } catch (e: HttpException) {
                 val errorBody = e.response()?.errorBody()?.string()
                 errorBody?.let {
-                    val jsonElement = JsonParser.parseString(it)
-                    val detail = jsonElement
-                        .asJsonObject["message"]
-                        .asString
-                    emit(ApiResponse.Error(detail))
+                    try {
+                        val parsedError = Gson().fromJson(errorBody, LoginResponse::class.java)
+                        if (parsedError != null) {
+                            emit(ApiResponse.Error(parsedError.message))
+                        } else {
+                            emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x0)"))
+                        }
+                    } catch (e: JsonSyntaxException) {
+                        emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x1)"))
+                    }
+                } ?: run {
+                    emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x2)"))
                 }
+            } catch (e: SocketTimeoutException) {
+                emit(ApiResponse.Error("Gagal terhubung, waktu tunggu koneksi habis"))
             } catch (e: Exception) {
-                e.message?.let {
-                    if (it == "timeout") emit(ApiResponse.Error("Connection timeout"))
-                }
-                emit(ApiResponse.Error("Something went wrong"))
+                emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x3)"))
             }
         }.flowOn(Dispatchers.IO)
     }
@@ -69,17 +84,23 @@ class RemoteDataSource(private val apiServices: ApiServices) {
             } catch (e: HttpException) {
                 val errorBody = e.response()?.errorBody()?.string()
                 errorBody?.let {
-                    val jsonElement = JsonParser.parseString(it)
-                    val detail = jsonElement
-                        .asJsonObject["message"]
-                        .asString
-                    emit(ApiResponse.Error(detail))
+                    try {
+                        val parsedError = Gson().fromJson(errorBody, LogoutResponse::class.java)
+                        if (parsedError != null) {
+                            emit(ApiResponse.Error(parsedError.message))
+                        } else {
+                            emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x0)"))
+                        }
+                    } catch (e: JsonSyntaxException) {
+                        emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x1)"))
+                    }
+                } ?: run {
+                    emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x2)"))
                 }
+            } catch (e: SocketTimeoutException) {
+                emit(ApiResponse.Error("Gagal terhubung, waktu tunggu koneksi habis"))
             } catch (e: Exception) {
-                e.message?.let {
-                    if (it == "timeout") emit(ApiResponse.Error("Connection timeout"))
-                }
-                emit(ApiResponse.Error("Something went wrong"))
+                emit(ApiResponse.Error("Terjadi sebuah kesalahan (0x3)"))
             }
         }.flowOn(Dispatchers.IO)
     }
