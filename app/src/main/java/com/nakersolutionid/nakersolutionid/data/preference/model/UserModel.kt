6e48e3a4d9c5bcf0948eeaec8552ba48f6d0ace8
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-06-30 11:35:56 +0700

    feat: added toUserDomain extension

--- app/src/main/java/com/nakersolutionid/nakersolutionid/data/preference/model/UserModel.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/data/preference/model/UserModel.kt
@@ -1,8 +1,19 @@
 package com.nakersolutionid.nakersolutionid.data.preference.model
 
+import com.nakersolutionid.nakersolutionid.domain.model.User
+
 data class UserModel(
     val id: String,
     val name: String,
     val username: String,
     val token: String
 )
+
+fun UserModel.toUserDomain(): User {
+    return User(
+        id = id,
+        name = name,
+        username = username,
+        token = token
+    )
+}
