fa03327386d5b35bc520bbf8bbc37fc22d5c0ff8
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-06-30 11:39:21 +0700

    feat: added user update and validate token jwt api call

--- app/src/main/java/com/nakersolutionid/nakersolutionid/data/remote/network/ApiServices.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/data/remote/network/ApiServices.kt
@@ -2,12 +2,17 @@ package com.nakersolutionid.nakersolutionid.data.remote.network
 
 import com.nakersolutionid.nakersolutionid.data.remote.request.LoginRequest
 import com.nakersolutionid.nakersolutionid.data.remote.request.RegisterRequest
+import com.nakersolutionid.nakersolutionid.data.remote.request.UpdateUserRequest
+import com.nakersolutionid.nakersolutionid.data.remote.request.ValidateTokenRequest
 import com.nakersolutionid.nakersolutionid.data.remote.response.login.LoginResponse
 import com.nakersolutionid.nakersolutionid.data.remote.response.logout.LogoutResponse
 import com.nakersolutionid.nakersolutionid.data.remote.response.register.RegisterResponse
+import com.nakersolutionid.nakersolutionid.data.remote.response.updateuser.UpdateUserResponse
+import com.nakersolutionid.nakersolutionid.data.remote.response.validatetoken.ValidateTokenResponse
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.PUT
 
 interface ApiServices {
     @POST("auth/register")
@@ -24,4 +29,15 @@ interface ApiServices {
     suspend fun logout(
         @Header("Authorization") token: String
     ) : LogoutResponse
+
+    @PUT("auth/update")
+    suspend fun updateUser(
+        @Header("Authorization") token: String,
+        @Body request: UpdateUserRequest
+    ) : UpdateUserResponse
+
+    @POST("auth/validateToken")
+    suspend fun validateToken(
+        @Body request: ValidateTokenRequest
+    ) : ValidateTokenResponse
 }
