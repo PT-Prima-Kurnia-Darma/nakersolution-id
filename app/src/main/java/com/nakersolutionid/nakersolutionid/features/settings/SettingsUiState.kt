44d0b76ee64018b402843e55d282ebe098f987a7
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-06-30 16:09:16 +0700

    feat: added more ui state for update user password

--- app/src/main/java/com/nakersolutionid/nakersolutionid/features/settings/SettingsUiState.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/features/settings/SettingsUiState.kt
@@ -4,8 +4,23 @@ import com.nakersolutionid.nakersolutionid.data.Resource
 
 data class SettingsUiState(
     val name: String = "",
+
     val username: String = "",
+
+    val oldPassword: String = "",
+    val newPassword: String = "",
+    val confirmNewPassword: String = "",
+
+    val isOldPasswordVisible: Boolean = false,
+    val isNewPasswordVisible: Boolean = false,
+    val isConfirmNewPasswordVisible: Boolean = false,
+
     val isLoading: Boolean = false,
+
+    val oldPasswordError: String? = null,
+    val newPasswordError: String? = null,
+    val confirmNewPasswordError: String? = null,
+
     val settingsResult: Resource<String>? = null,
     val logoutResult: Resource<String>? = null
 )
