91c93a9daa18d1086bdb51cb25a52b233bc6a2be
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-07-05 00:18:10 +0700

    feat: try implement saving into local database

--- app/src/main/java/com/nakersolutionid/nakersolutionid/data/local/LocalDataSource.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/data/local/LocalDataSource.kt
@@ -1,7 +1,8 @@
 package com.nakersolutionid.nakersolutionid.data.local
 
 import com.nakersolutionid.nakersolutionid.data.local.dao.ReportDao
+import com.nakersolutionid.nakersolutionid.data.local.entity.ReportEntity
 
 class LocalDataSource(private val reportDao: ReportDao) {
-
+    suspend fun insertReport(report: ReportEntity) = reportDao.insertReport(report)
 }
