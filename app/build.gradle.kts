2892143e1e003dc7d1d74317f7f88f38e91044f4
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-07-04 15:55:35 +0700

    feat: update material compose 3 into 1.4.0 alpha version. careful

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.runtime.livedata)
+    implementation(libs.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
