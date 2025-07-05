b35c27d3425ba26e72f769e7e575ca6c1871301e
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-07-05 18:55:50 +0700

    fix: update response

--- app/src/main/java/com/nakersolutionid/nakersolutionid/data/remote/response/sendreport/Data.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/data/remote/response/sendreport/Data.kt
@@ -5,14 +5,8 @@ import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 @Parcelize
-data class SendReportResponse(
+data class Data(
 
-	@field:SerializedName("data")
-	val data: Data,
-
-	@field:SerializedName("message")
-	val message: String,
-
-	@field:SerializedName("status")
-	val status: String
+	@field:SerializedName("laporan")
+	val laporan: Laporan
 ) : Parcelable
