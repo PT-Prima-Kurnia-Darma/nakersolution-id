3d1e5b6fb4ec1d725877d28894636269f700f475
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-07-06 21:40:14 +0700

    feat: added fix type for report

---
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/data/local/utils/Type.kt
@@ -0,0 +1,53 @@
+package com.nakersolutionid.nakersolutionid.data.local.utils
+
+enum class DocumentType {
+    LAPORAN,
+    BAP,
+    SURAT_KETERANGAN_SEMENTARA,
+    SERTIFIKAT_SEMENTARA
+}
+
+enum class ReportType {
+    ILPP,
+    IPK,
+    PAA,
+    PUBT,
+    PTP,
+    EE
+}
+
+enum class SubReportType {
+    Elevator,
+    Eskalator,
+    Travelator,
+
+    Forklift,
+    Bulldozer,
+    Excavator,
+    Whell_Loader,
+    Farm_Tractor,
+    Motor_Grader,
+    Mobil_Crane_Truck_Crane,
+    Hoist_Crane_Overhead_Crane_Gantry_Crane_Gondola_Jib_Crane,
+    Tower_Crane,
+
+    Instalasi_Petir,
+    Instalasi_Penyalur_Petir,
+
+    Ketel_Uap,
+    Bejana_Uap,
+    BPV,
+    Tangki_Timbun,
+    Compressor_BT,
+    Pipa_Bertenaga_Bertekanan,
+
+    Alarm_Kebakaran_Otomatis,
+    Instalasi_Hydrant_dan_Sprinkle,
+    Instalasi_Floading_System,
+
+    Motor_Diesel,
+    Turbin,
+    Perkakas,
+    Produksi,
+    Tanur
+}
