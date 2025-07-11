7ccdee057ad8e0635008a16e07117856cd1c694a
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-07-11 09:45:01 +0700

    feat: added DI on ilpp viewmodel

--- app/src/main/java/com/nakersolutionid/nakersolutionid/features/report/ilpp/ILPPViewModel.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/features/report/ilpp/ILPPViewModel.kt
@@ -1,7 +1,8 @@
 package com.nakersolutionid.nakersolutionid.features.report.ilpp
 
 import androidx.lifecycle.ViewModel
+import com.nakersolutionid.nakersolutionid.domain.usecase.ReportUseCase
 
-class ILPPViewModel : ViewModel() {
+class ILPPViewModel(private val reportUseCase: ReportUseCase) : ViewModel() {
 
 }
