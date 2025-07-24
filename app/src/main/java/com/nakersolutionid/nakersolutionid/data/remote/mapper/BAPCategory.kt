b18dee2e1d5754f1615059fca936cbd6ba70e501
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-07-24 09:54:23 +0700

    feat: add BAPCategory object
    This commit introduces a new object `BAPCategory` in the `com.nakersolutionid.nakersolutionid.data.remote.mapper` package.
    
    The `BAPCategory` object defines constants for Berita Acara Pemeriksaan (BAP) categories:
    - `VISUAL_INSPECTION`: "PEMERIKSAAN VISUAL"
    - `TESTING`: "PENGUJIAN"
    
    These constants can be used to categorize different sections or types of inspections within a BAP report.

---
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/data/remote/mapper/BAPCategory.kt
@@ -0,0 +1,6 @@
+package com.nakersolutionid.nakersolutionid.data.remote.mapper
+
+object BAPCategory {
+    const val VISUAL_INSPECTION = "PEMERIKSAAN VISUAL"
+    const val TESTING = "PENGUJIAN"
+}
