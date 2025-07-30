3c47622edbd34bb19f5eda5d003c6468e44663d4
mhmdazkanfl <mhmdazkanfl@gmail.com>
2025-07-30 16:44:40 +0700

    refactor: add foreign key to RemoteKeyEntity
    This commit adds a foreign key constraint to the `RemoteKeyEntity`.
    The `inspectionId` in `RemoteKeyEntity` now references the `rowid` of the `InspectionEntity`.
    This ensures that if an `InspectionEntity` is deleted, the corresponding `RemoteKeyEntity` will also be deleted due to the `onDelete = ForeignKey.CASCADE` rule.

--- app/src/main/java/com/nakersolutionid/nakersolutionid/data/local/entity/RemoteKeyEntity.kt
+++ app/src/main/java/com/nakersolutionid/nakersolutionid/data/local/entity/RemoteKeyEntity.kt
@@ -1,9 +1,21 @@
 package com.nakersolutionid.nakersolutionid.data.local.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "remote_keys")
+@Entity(
+    tableName = "remote_keys",
+    // 👇 ADD THIS FOREIGN KEY DEFINITION
+    foreignKeys = [
+        ForeignKey(
+            entity = InspectionEntity::class,
+            parentColumns = ["rowid"],
+            childColumns = ["inspectionId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class RemoteKeyEntity(
     @PrimaryKey val inspectionId: Long,
     val prevKey: Int?,
